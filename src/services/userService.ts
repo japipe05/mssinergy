6810f641111fb3a1c38162a648e352ac3eda2ce4
Andres Felipe Rodriguez roa <felipehuchija@gmail.com>
2025-05-22 04:34:06 -0500

    1

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,10 @@
-import { RowDataPacket } from 'mysql2';
+
 import pool from '@/config/db';
-import { userData } from '@/models/userData';
-import { hashPassword, comparePasswords } from '@/utils/hash';
-import logger from '@/utils/logger';
+import { DBUser } from '@/models/usuarioData';
+
 
 export async function getUserById(id: number) {
-  const [rows] = await pool.query<userData[]>(
+  const [rows] = await pool.query<DBUser[]>(
     'SELECT usr_id, usr_nombre, usr_correo, usr_fotografia FROM usuarios WHERE usr_id = ?',
     [id]
   );
