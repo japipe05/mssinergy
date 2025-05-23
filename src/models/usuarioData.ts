926c3edc94364066f935ad9b3daba55252d42a20
Andres Felipe Rodriguez roa <felipehuchija@gmail.com>
2025-05-23 17:24:32 -0500

    1

--- src/models/usuarioData.ts
+++
@@ -1,8 +0,0 @@
-import { RowDataPacket } from 'mysql2';
-
-export interface DBUser extends RowDataPacket {
-  usr_id: number;
-  usr_nombre: string;
-  usr_correo: string;
-  usr_fotografia: string;
-}
