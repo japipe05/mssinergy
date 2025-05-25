ae6590e734a30ed415d76e45a2f0bbb562db9f5a
Andres Felipe Rodriguez roa <felipehuchija@gmail.com>
2025-05-24 20:10:51 -0500

    1
    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
+
 // Tu clave secreta
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET || 'secreto_dev');
 
